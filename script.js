fe1adb059ccbbd09f00d14a6d506ace4f0158f3f
Lucas Alexandre Neves <lucaslhneves@gmail.com>
2022-11-11 00:14:28 -0300

    Progress bar adjustment

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 // Initial Datta
 let currentQuestion = 0
+let correctAnswers = 0
 
 showQuestion()
 
@@ -8,6 +9,10 @@ showQuestion()
 function showQuestion(){
     if(questions[currentQuestion]){
         let q = questions[currentQuestion]
+
+        let pct = Math.floor((currentQuestion / questions.length) * 100)
+
+        document.querySelector('.progress--bar').style.width = `${pct}%`
 
         document.querySelector('.scoreArea').style.display = 'none'
         document.querySelector('.questionArea').style.display = 'block'
@@ -18,11 +23,25 @@ function showQuestion(){
         for(let i in q.options) {
             optionsHtml += `<div data-op="${i}" class="option"><span>${parseInt(i) + 1}</span> ${q.options[i]} </div>`
         }
-
         document.querySelector('.options').innerHTML = optionsHtml
+
+        document.querySelectorAll('.options .option').forEach(item => {
+            item.addEventListener('click', optionClickEvent)
+        })
 
 
     } else {
         //acabaram as questões
     }
+}
+
+function optionClickEvent(e) {
+    let clickedOption =parseInt(e.target.getAttribute('data-op')) 
+
+    if(questions[currentQuestion].answer == clickedOption) {
+        correctAnswers++
+    }
+
+    currentQuestion++
+    showQuestion()
 }
